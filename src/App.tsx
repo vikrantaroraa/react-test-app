30dc49626c019644fa945fe486f343f77006a8f6
vikrantaroraa <arora.veshu@gmail.com>
2023-07-20 20:17:37 +0530

    feat: added button to call api

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,14 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import styles from "./App.module.css";
 
 function App() {
+  const [apiResponse, setApiResponse] = useState("");
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={styles["App"]}>
+      <button onClick={() => setApiResponse("Hello World!")}>
+        Fetch Express Server
+      </button>
+      <div className={styles["response-output"]}>{apiResponse}</div>
     </div>
   );
 }
