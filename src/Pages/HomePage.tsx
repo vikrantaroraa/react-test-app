d7651b3419724ca45063c4b5a362010310cd3ced
vikrantaroraa <arora.veshu@gmail.com>
2023-07-22 21:08:10 +0530

    feat: changes pages folder name from Pages to pages

--- src/Pages/HomePage.tsx
+++ src/Pages/HomePage.tsx
@@ -1,7 +1,13 @@
 import React from "react";
 
 function HomePage() {
-  return <div>HomePage</div>;
+  return (
+    <div>
+      In this commit. i have changed the text of this div and also renamed the
+      pages folder name from "Pages" to "pages" i.e. from capital "P" to small
+      "p" but github does not recognize this change.
+    </div>
+  );
 }
 
 export default HomePage;
