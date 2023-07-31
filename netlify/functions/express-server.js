71a9404a4be84275b9f6e924c902ea94fc46a8f2
vikrantaroraa <arora.veshu@gmail.com>
2023-07-31 18:44:18 +0530

    fix: fixed api spelling

--- netlify/functions/express-server.js
+++ netlify/functions/express-server.js
@@ -21,7 +21,7 @@ router.get("/get-api-key", (req, res) => {
   res.json({
     message: "sending api key - from express!",
     key: `the api key is: ${process.env.SECRET_KEY}`,
-    key_2: `the pai key imported from env file is: ${process.env.SECRET_KEY_FROM_ENV_FILE}`,
+    key_2: `the api key imported from env file is: ${process.env.SECRET_KEY_FROM_ENV_FILE}`,
   });
 });
 
